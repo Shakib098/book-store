85a24a9f0f2df470647f4fa89da2c978a8321ea0
Juhaib Khan <juhaibkhanbca2k13@gmail.com>
2023-04-07 20:36:37 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes")
+const cors = require("cors")
+const app = express()
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+app.use("/books", router) // localhost:5000/books
+
+mongoose.connect("mongodb+srv://shakib:8Rn7PzHEDKsZDCVX@cluster0.uyrmsgt.mongodb.net/Book_Store?retryWrites=true&w=majority"
+)
+.then(() => console.log("Connected To Database"))
+.then(() => {
+    app.listen(5000);
+})
+.catch((err) => console.log(err))
+
+//8Rn7PzHEDKsZDCVX
